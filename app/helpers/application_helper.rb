c4a075d538ec6f4c2ed509fbd20f7a9414eaa692
Felipe Pessoa <felipe.pessoa@sefin.ro.gov.br>
2020-03-18 09:23:34 -0400

    add first contentful request

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
+  require 'contentful'
+
   def contentful
-    @client ||= Contentful::Client.new(
-        access_token: ENV['e1FcWyWu3oEn08XxHZvUHohTxRq06T2fP7wWBQFVn5g'],
-        space: ENV['h0hn2pnr1nct'],
-        dynamic_entries: :auto,
-        raise_errors: true
+    @client = Contentful::Client.new(
+        access_token: 'pGry8uBgtxMqy8Hhsk12sGDOsqWpnDc4DliWHpXuQ8w',
+        space: 'h0hn2pnr1nct'
     )
   end
 end
