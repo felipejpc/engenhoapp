b7742f4b3d02690554eb974b9595f819782b9ab9
Felipe Pessoa <felipejpc@yahoo.com.br>
2020-03-29 16:18:49 -0400

    Defined root. Deleted useless routes and respective methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  resources :posts, param: :slug
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :posts, param: :slug, only: [:index, :show]
+  root 'posts#index'
 end
