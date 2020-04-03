eff1d523cda56887c0ec4c2e86d5e1d5b0c5b0ce
Felipe Pessoa <felipejpc@yahoo.com.br>
2020-04-03 11:29:01 -0400

    Add subdomain on posts and pages routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :pages, param: :slug, only: [:show]
-  resources :posts, param: :slug, only: [:index, :show]
+  constraints subdomain: 'blog' do
+    resources :pages, param: :slug, only: [:show]
+    resources :posts, param: :slug, only: [:index, :show]
+  end
   root 'posts#index'
 end
