98d26305a97886917e46ff8f77b19b93016a9e8c
Felipe Pessoa <felipejpc@yahoo.com.br>
2020-04-07 06:15:12 -0400

    Config layout js file loads and delete useless css files

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,8 +4,9 @@
 // that code so it'll be compiled.
 //= require turbolinks
 //= require jquery-2.2.0.min
-//= require_tree .
-
+//= require bootstrap.min
+//= require jquery-plugin-collection
+//= require jquery-ui.min
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
