dc9bda4bfb0b952bbd86ccdec921dc42afd00ce1
Felipe Pessoa <felipejpc@yahoo.com.br>
2020-04-19 06:05:37 -0400

    Config blog page view for use rich text. Change rich text helper from posts helper to application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  require 'rich_text_renderer'
+
+  def rich_text_render(content)
+    renderer = RichTextRenderer::Renderer.new
+    renderer.render(content)
+  end
 end
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,10 +1,4 @@
 # frozen_string_literal: true
 
 module PostsHelper
-  require 'rich_text_renderer'
-
-  def rich_text_render(content)
-    renderer = RichTextRenderer::Renderer.new
-    renderer.render(content)
-  end
 end
